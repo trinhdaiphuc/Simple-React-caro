75a8462e04abe079d1debd5c075cd8e38150f782
Trinh Dai Phuc <1653068@student.hcmus.edu.vn>
2020-05-29 16:03:09 +0700

    Updates

---
+++ precache-manifest.da1e63e9e1e422661f6f75c278e56292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c55cad6c5072dcea6b5579dd26a96f",
+    "url": "/Simple-React-caro/index.html"
+  },
+  {
+    "revision": "f1e6519e2b2ce8d1c991",
+    "url": "/Simple-React-caro/static/css/main.de1713d7.chunk.css"
+  },
+  {
+    "revision": "d27105349ea2ca9fcc3b",
+    "url": "/Simple-React-caro/static/js/2.64c609ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-React-caro/static/js/2.64c609ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e6519e2b2ce8d1c991",
+    "url": "/Simple-React-caro/static/js/main.09acba9b.chunk.js"
+  },
+  {
+    "revision": "e514a3a68cd93c7c5bc4",
+    "url": "/Simple-React-caro/static/js/runtime-main.127ae498.js"
+  }
+]);
